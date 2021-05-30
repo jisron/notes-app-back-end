e98ad2572136b98a053946864b3e6ac6f16626f5
jisron <jisron94@gmail.com>
2021-05-30 11:20:46 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,8 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+/* eslint-disable eol-last */
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable no-trailing-spaces */
+const notes = [];
+ 
+module.exports = notes;
